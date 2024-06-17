dc78c5dc63b0c0c38c16691c4d5870345dee01f1
BernieMacHub <jakevalleleyjohnson@outlook.com>
2024-06-17 16:47:00 +0100

    Add function structure

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,37 @@
 console.log("Connected!")
+
+function runGame() {
+
+}
+
+function checkAnswer() {
+
+}
+
+function calcuateCorrectAnswer() {
+
+}
+
+function incrementScore() {
+
+}
+
+function incrementWrongAnswer() {
+
+}
+
+function displayAdditionQuestion() {
+
+}
+
+function displaySubtractQuestion() {
+
+}
+
+function displayMultiplyQuestion() {
+
+}
+
+function displayDivideQuestion() {
+
+}
